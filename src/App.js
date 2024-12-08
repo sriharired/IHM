13c75b7a36a4ca712962581a7af040deb9711f3f
sriharired <psrihari1952@gmail.com>
2024-12-08 15:24:41 -0500

    Updated Successfully

--- src/App.js
+++ src/App.js
@@ -1,40 +1,65 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Header from "../src/components/header";
-import Home from "../src/pages/home";
-import About from "../src/pages/about";
-import Histories from "../src/pages/history";
-import QuizDetailList from "../src/pages/quizdetail";
-import QuizQuestions from "./pages/quizquestions";
-import HistoryDetails from "./pages/historydetails";
-import PartsDetails from "./pages/partsdetails";
-import CharacterPage from "./pages/characterdetails";
-import QuizList from "../src/pages/quiz";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Histories from "./pages/Histories";
+import QuizList from "./pages/QuizList";
+import QuizDetailList from "./pages/QuizDetailList";
+import QuizQuestions from "./pages/QuizQuestions";
+import HistoryDetails from "./pages/HistoryDetails";
+import PartsDetails from "./pages/PartsDetails";
+import CharacterPage from "./pages/CharacterPage";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-const App = () => {
+function App() {
   useEffect(() => {
     document.title = "IHM - Indian History Mythologies";
   }, []);
 
   return (
-    <Router basename="/IHM">
-      <div className="app">
+    <Router>
+      <div className="App">
         <Header />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={
+              <div>
+                <section id="home">
+                  <Home />
+                </section>
+                <section id="about">
+                  <About />
+                </section>
+                <section id="histories">
+                  <Histories />
+                </section>
+                <section id="quizlist">
+                  <QuizList />
+                </section>
+              </div>
+            }
+          />
           <Route path="/about" element={<About />} />
           <Route path="/histories" element={<Histories />} />
           <Route path="/histories/:id" element={<HistoryDetails />} />
-          <Route path="/histories/:id/parts/:partTitle" element={<PartsDetails />} />
-          <Route path="/histories/:id/characters/:characterName" element={<CharacterPage />} />
+          <Route
+            path="/histories/:id/parts/:partTitle"
+            element={<PartsDetails />}
+          />
+          <Route
+            path="/histories/:id/characters/:characterName"
+            element={<CharacterPage />}
+          />
           <Route path="/quiz" element={<QuizList />} />
           <Route path="/quiz/:category" element={<QuizDetailList />} />
           <Route path="/quiz/:category/:quizName" element={<QuizQuestions />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
-};
+}
 
 export default App;
